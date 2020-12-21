b0c291fc1ae3bbda1efaef788e26a21e8e279130
Andrii Potapenko <andriy.potapenko@trov.com>
2020-12-21 12:57:30 +0000

    Fix camera issues on legacy Android API

--- uikit/src/main/java/datatouch/uikit/components/camera/activities/PhotoCameraActivity.kt
+++ uikit/src/main/java/datatouch/uikit/components/camera/activities/PhotoCameraActivity.kt
@@ -40,25 +40,17 @@ class PhotoCameraActivity : AppCompatActivity() {
     private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
     private lateinit var activityParams: PhotoCameraActivityParams
 
-    private val cameraProvider by lazy {
-        ProcessCameraProvider.getInstance(this)
-    }
+    private val cameraProvider by lazy { ProcessCameraProvider.getInstance(this) }
 
-    private val executor by lazy {
-        ContextCompat.getMainExecutor(this)
-    }
+    private val executor by lazy { ContextCompat.getMainExecutor(this) }
 
     private val metadata by lazy {
         packageManager.getActivityInfo(componentName, PackageManager.GET_META_DATA).metaData
     }
 
-    private val permissions by lazy {
-        listOf(Manifest.permission.CAMERA)
-    }
+    private val permissions by lazy { listOf(Manifest.permission.CAMERA) }
 
-    private val permissionsRequestCode by lazy {
-        Random.nextInt(0, 10000)
-    }
+    private val permissionsRequestCode by lazy { Random.nextInt(0, 10000) }
 
     private fun getConfigurationValue(key: String): Any? = when {
         intent.extras?.containsKey(key) == true -> intent.extras?.get(key)
@@ -78,7 +70,8 @@ class PhotoCameraActivity : AppCompatActivity() {
     }
 
     private fun injectActivityParams() {
-        val params = intent.extras?.getSerializable(PhotoCameraActivityParams.Key) as? PhotoCameraActivityParams?
+        val params =
+            intent.extras?.getSerializable(PhotoCameraActivityParams.Key) as? PhotoCameraActivityParams?
         params?.let { activityParams = it } ?: run {
             activityParams = PhotoCameraActivityParams(filesDir)
         }
@@ -142,25 +135,22 @@ class PhotoCameraActivity : AppCompatActivity() {
         setResult(Activity.RESULT_OK, Intent().apply {
             putExtra(activityParams.key, CameraActivityResult(photoFile))
         })
+
         finish()
     }
 
     private fun setupSwitchCameraListener() {
         btnSwitchCamera?.setOnClickListener {
-
-            // Flip-flop the required lens facing
-            lensFacing = if (CameraSelector.LENS_FACING_FRONT == lensFacing) {
+            lensFacing = if (CameraSelector.LENS_FACING_FRONT == lensFacing)
                 CameraSelector.LENS_FACING_BACK
-            } else {
+            else
                 CameraSelector.LENS_FACING_FRONT
-            }
 
-            bindCameraUseCases()
+            startCamera()
         }
     }
 
     private fun applyUserConfiguration() {
-
         // If the user requested a specific lens facing, select it
         getConfigurationValue(CameraConfiguration.CAMERA_LENS_FACING)?.let {
             lensFacing = it as Int
@@ -177,47 +167,45 @@ class PhotoCameraActivity : AppCompatActivity() {
         finish()
     }
 
-    /** Declare and bind preview, capture and analysis use cases */
-    private fun bindCameraUseCases() = previewView?.post {
-        cameraProvider.addListener(Runnable {
+    private fun startCamera() {
+        val startCameraRunnable = {
+            cameraProvider.addListener({
+                val cameraProvider = cameraProvider.get()
 
-            // Camera provider is now guaranteed to be available
-            val cameraProvider = cameraProvider.get()
+                preview = Preview.Builder()
+                    .setTargetRotation(previewView?.display?.rotation.orZero())
+                    .build()
+                    .apply { setSurfaceProvider(previewView?.surfaceProvider) }
 
-            // Set up the view finder use case to display camera preview
-            preview = Preview.Builder()
-                .setTargetRotation(previewView?.display?.rotation.orZero())
-                .build()
-                .apply { setSurfaceProvider(previewView?.surfaceProvider) }
+                imageCapture = ImageCapture.Builder()
+                    .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                    .setTargetRotation(previewView?.display?.rotation.orZero())
+                    .build()
 
-            // Set up the capture use case to allow users to take photos
-            imageCapture = ImageCapture.Builder()
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                .setTargetRotation(previewView?.display?.rotation.orZero())
-                .build()
+                val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
 
-            // Create a new camera selector each time, enforcing lens facing
-            val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+                cameraProvider.unbindAll()
+                cameraProvider
+                    .bindToLifecycle(this as LifecycleOwner, cameraSelector, preview, imageCapture)
+            }, executor)
+        }
 
-            // Apply declared configs to CameraX using the same lifecycle owner
-            cameraProvider.unbindAll()
-            cameraProvider.bindToLifecycle(
-                this as LifecycleOwner, cameraSelector, preview, imageCapture
-            )
-        }, executor)
+        // Prevent 'Could not retrieve native window from surface' error on legacy API
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1)
+            previewView?.post(startCameraRunnable)
+        else
+            previewView?.postDelayed(startCameraRunnable, LegacyCameraStartDelayMs)
+
     }
 
     override fun onResume() {
         super.onResume()
+        if (!hasPermissions(this))
+            ActivityCompat
+                .requestPermissions(this, permissions.toTypedArray(), permissionsRequestCode)
+        else
+            startCamera()
 
-        // Request permissions each time the app resumes, since they can be revoked at any time
-        if (!hasPermissions(this)) {
-            ActivityCompat.requestPermissions(
-                this, permissions.toTypedArray(), permissionsRequestCode
-            )
-        } else {
-            bindCameraUseCases()
-        }
     }
 
     override fun onRequestPermissionsResult(
@@ -226,12 +214,10 @@ class PhotoCameraActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == permissionsRequestCode && hasPermissions(this)) {
-            bindCameraUseCases()
-        } else {
-            // Indicate that the user cancelled the action and exit if no permissions are granted
+        if (requestCode == permissionsRequestCode && hasPermissions(this))
+            startCamera()
+        else
             cancelAndFinish()
-        }
     }
 
     override fun onBackPressed() {
@@ -244,3 +230,5 @@ class PhotoCameraActivity : AppCompatActivity() {
     }
 
 }
+
+private const val LegacyCameraStartDelayMs = 1000L
