388f4b021424b03ce4904d0fc675beb668d1312b
Andrii Potapenko <andriy.potapenko@trov.com>
2020-12-21 16:26:49 +0000

    Enable toast from java calls

--- uikit/src/main/java/datatouch/uikit/components/toast/ToastNotification.kt
+++ uikit/src/main/java/datatouch/uikit/components/toast/ToastNotification.kt
@@ -6,34 +6,42 @@ import es.dmoral.toasty.Toasty.*
 
 object ToastNotification {
 
+    @JvmStatic
     fun showWarning(ctx: Context?, message: String?) = ctx?.apply {
         warning(ctx, message ?: "", Toast.LENGTH_LONG, true).show()
     }
 
+    @JvmStatic
     fun showWarning(activity: Context?, message: Int) = activity?.apply {
         warning(this, resources.getString(message), Toast.LENGTH_LONG, true).show()
     }
 
+    @JvmStatic
     fun showError(ctx: Context?, message: String?) = ctx?.apply {
         error(this, message ?: "").show()
     }
 
+    @JvmStatic
     fun showError(ctx: Context?, message: Int) = ctx?.apply {
         error(this, resources.getString(message)).show()
     }
 
+    @JvmStatic
     fun showSuccess(ctx: Context?, message: String?) = ctx?.apply {
         success(this, message ?: "").show()
     }
 
+    @JvmStatic
     fun showSuccess(ctx: Context?, message: Int) = ctx?.apply {
         success(this, resources.getString(message)).show()
     }
 
+    @JvmStatic
     fun showInfo(ctx: Context?, message: String?) = ctx?.apply {
         info(this, message ?: "").show()
     }
 
+    @JvmStatic
     fun showInfo(ctx: Context?, message: Int) = ctx?.apply {
         info(this, resources.getString(message)).show()
     }
