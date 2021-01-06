66b582ae48480abc468bcdfa2129b911aaa2e3d6
andrii.potapenko <andrii.potapenko@datascopeplc.com>
2021-01-06 13:10:07 +0000

    Add ImageView extensions

--- uikit/src/main/java/datatouch/uikit/core/extensions/ImageViewExtensions.kt
+++ uikit/src/main/java/datatouch/uikit/core/extensions/ImageViewExtensions.kt
@@ -1,13 +1,19 @@
 package datatouch.uikit.core.extensions
 
 import android.widget.ImageView
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import datatouch.uikit.R
 import datatouch.uikit.core.utils.imaging.ImageUtils
 import java.io.File
 
 object ImageViewExtensions {
 
-    fun ImageView.showImageResource(resId: Int) =
+    fun ImageView.setColorFilterResource(@ColorRes resId: Int) =
+        this.setColorFilter(ContextCompat.getColor(context, resId))
+
+    fun ImageView.showImageResource(@DrawableRes resId: Int) =
         ImageUtils.setImageResource(resId, this)
 
     fun ImageView.showLocalImage(imagePath: String?, errorResId: Int = R.drawable.image_not_found) =
