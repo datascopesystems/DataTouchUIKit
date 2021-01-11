d643d032fc17291bb4cbec88e45f593e049a6906
Andrii Potapenko <andriy.potapenko@trov.com>
2021-01-11 13:32:25 +0000

    Add TextViewExtensions

---
+++ uikit/src/main/java/datatouch/uikit/core/extensions/TextViewExtensions.kt
@@ -0,0 +1,19 @@
+package datatouch.uikit.core.extensions
+
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+import datatouch.uikit.core.utils.Conditions
+
+
+object TextViewExtensions {
+
+    inline fun TextView.setText(text: String?, @StringRes defaultResId: Int) =
+        if (Conditions.isNotNullOrEmpty(text)) this.text = text
+        else setText(defaultResId)
+
+    inline fun TextView.setTextColorResource(@ColorRes colorRes: Int) =
+        setTextColor(ContextCompat.getColor(context, colorRes))
+
+}
