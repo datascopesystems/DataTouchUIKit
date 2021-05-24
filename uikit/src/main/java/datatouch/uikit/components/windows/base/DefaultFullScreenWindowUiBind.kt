271357b2c05d180794f072fec136e759204e1bcf
andrii.potapenko <andrii.potapenko@datascopeplc.com>
2021-05-24 12:25:32 +0100

    Add base window classes, wizard classes

---
+++ uikit/src/main/java/datatouch/uikit/components/windows/base/DefaultFullScreenWindowUiBind.kt
@@ -0,0 +1,31 @@
+package datatouch.uikit.components.windows.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import datatouch.uikit.core.extensions.ViewBindingExtensions.getViewBindingClass
+import net.matsudamper.viewbindingutil.ViewBindingUtil
+
+
+abstract class DefaultFullScreenWindowUiBind<TFragmentLayout : ViewBinding, TToolbar : View>
+    : FullScreenWindow<TToolbar>() {
+
+    var ui: TFragmentLayout? = null
+        private set
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        ui = ViewBindingUtil.inflate(
+            inflater, container, false, getViewBindingClass(javaClass))
+        return ui?.root
+    }
+
+    override fun onDestroyView() {
+        ui = null
+        super.onDestroyView()
+    }
+}
