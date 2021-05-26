4965dce1c3b3f4f3641362eebad6c30e76ba98bd
andrii.potapenko <andrii.potapenko@datascopeplc.com>
2021-05-26 10:58:33 +0100

    Fix Environment.kt

--- uikit/src/main/java/datatouch/uikit/components/Environment.kt
+++ uikit/src/main/java/datatouch/uikit/components/Environment.kt
@@ -3,5 +3,12 @@ package datatouch.uikit.components
 import datatouch.uikit.BuildConfig
 
 object Environment {
-    val isDebug get() = BuildConfig.DEBUG
+
+    private var isDebugEnv: Boolean? = null
+
+    fun setIsDebug(isDebug: Boolean) {
+        isDebugEnv = isDebug
+    }
+
+    val isDebug: Boolean get() = isDebugEnv?.apply {} ?: BuildConfig.DEBUG
 }
