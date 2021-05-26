207173dbfe2028c3b89ef27b96c1de79f902dda0
andrii.potapenko <andrii.potapenko@datascopeplc.com>
2021-05-26 16:29:53 +0100

    Fix ViewBindingExtensions

--- uikit/src/main/java/datatouch/uikit/core/extensions/ViewBindingExtensions.kt
+++ uikit/src/main/java/datatouch/uikit/core/extensions/ViewBindingExtensions.kt
@@ -7,15 +7,8 @@ import java.lang.reflect.ParameterizedType
 object ViewBindingExtensions {
 
     @Suppress("UNCHECKED_CAST")
-    fun <TLayout : ViewBinding> getViewBindingClass(viewClass: Class<Any>): Class<TLayout> {
-        return if (viewClass.name.endsWith("_")) { // Workaround for Android Annotations
-            val superClass = viewClass.superclass
-            val genericSuperclass = superClass.genericSuperclass
-            (genericSuperclass as ParameterizedType).actualTypeArguments[0] as Class<TLayout>
-        } else {
-            return getBindingClassName(viewClass)
-        }
-    }
+    fun <TLayout : ViewBinding> getViewBindingClass(viewClass: Class<Any>): Class<TLayout> =
+        getBindingClassName(viewClass)
 
     private fun <TLayout : ViewBinding> getBindingClassName(viewClass: Class<Any>): Class<TLayout> {
         if (viewClass.isNull()) throw IllegalArgumentException("Binding class name was not found in class generic hierarchy")
